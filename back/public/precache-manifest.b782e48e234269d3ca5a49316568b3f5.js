edc438987ac882958a399662e94e8e51899e0ff4
sanidhya-saraswat <sanidhya.saraswat@gmail.com>
2019-03-04 12:31:58 +0530

    x

---
+++ back/public/precache-manifest.b782e48e234269d3ca5a49316568b3f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a216f42669b96bfe6c9f",
+    "url": "/static/js/main.a216f426.chunk.js"
+  },
+  {
+    "revision": "54aeec09d6e3bb765b8d",
+    "url": "/static/js/1.54aeec09.chunk.js"
+  },
+  {
+    "revision": "a216f42669b96bfe6c9f",
+    "url": "/static/css/main.db1bea06.chunk.css"
+  },
+  {
+    "revision": "8873439dabaf4d46de026e1b511ee8ca",
+    "url": "/index.html"
+  }
+];
