2e84428a447bfb700a7c675fad3370e86d249e00
sanidhya-saraswat <sanidhya.saraswat@gmail.com>
2019-03-04 14:20:21 +0530

    x

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express= require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const path=require('path');
-var app=express();
+const path = require('path');
+var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-const port=process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log(`Express server listening on port ${port}`);
+// Handle React routing, return all requests to React app
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+const port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log(`Express server listening on port ${port}`);
 });
