0728a8ab8bec39915e0053ea864bbc7208127c20
sanidhya-saraswat <sanidhya.saraswat@gmail.com>
2019-03-04 13:05:15 +0530

    x

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 const port=process.env.PORT || 5000;
 app.listen(port, function() {
